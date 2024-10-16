929eb7f2677f7f463faad89ed941c11e155ec741
CMS1124 <chooe8239@gmail.com>
2024-10-17 03:32:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
-from flask import Flask, request, jsonify, send_file
-import os
+from flask import Flask, request, render_template
 from pytube import YouTube
 
 app = Flask(__name__)
 
-@app.route('/download', methods=['POST'])
-def download_video():
-    data = request.get_json()
-    youtube_url = data.get('url')
-
-    if not youtube_url:
-        return jsonify({'success': False, 'error': 'No URL provided'})
-
-    try:
-        yt = YouTube(youtube_url)
-        stream = yt.streams.get_highest_resolution()  # 가장 높은 해상도의 영상 다운로드
-        download_path = stream.download()
-
-        return jsonify({'success': True, 'downloadUrl': f'/files/{os.path.basename(download_path)}'})
-    except Exception as e:
-        return jsonify({'success': False, 'error': str(e)})
-
-@app.route('/files/<filename>')
-def serve_file(filename):
-    return send_file(filename, as_attachment=True)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['url']
+        yt = YouTube(url)
+        yt.streams.first().download()  # 첫 번째 스트림 다운로드
+        return f'Downloaded: {yt.title}'
+    return '''
+        <form method="post">
+            YouTube URL: <input type="text" name="url">
+            <input type="submit" value="Download">
+        </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
